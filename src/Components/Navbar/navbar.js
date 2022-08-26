5b371081b2536217750b9f84a45a5c1217bfa1b1
prithviya <94467336+prithviya@users.noreply.github.com>
2022-08-26 10:06:07 +0530

    2commit

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -73,6 +73,27 @@ function navbar() {
 
             </div>
         </nav>
+        <button
+                onClick={() => {
+                window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+                }}
+                style={{
+                position: 'fixed',
+                padding: '7px 7px',
+                fontSize: '2rem !important',
+                bottom: '40px',
+                right: '40px',
+                background: 'linear-gradient(19deg, #ff4500 10%, #d43b04 100%)',
+                // backgroundColor: 'rgb(212, 59, 4)',
+                // background: url('..'),
+                color: '#fff',
+                textAlign: 'center',
+                zIndex: '9999',
+                borderRadius: '5px',
+                }}
+            >
+                <img src="https://img.icons8.com/ios-filled/20/FFFFFF/login-rounded-up.png"/>
+            </button>
     </div>
   )
 }
